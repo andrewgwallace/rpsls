97647e61b0f35f2624671aa51a39eb9ad27eaf82
johntrandall <john@johnrandall.com>
2014-05-06 13:42:27 -0400

    Your Name: John Randall
    Subject Line:
    added style reset.
    Dropped in some example code in main.rb
    
    What Happened:

--- app.rb
+++ app.rb
@@ -21,6 +21,33 @@ $wins_hash = {
 #### LOGIC
 
 
+            # ######### Example from morning exercise - for example purposes only.
+
+            # def computer_choses
+            #   computer_choice = $possible_choices[rand(4)-1]
+            #   return computer_choice
+            # end
+
+
+            # def evaluate_results(user_choice, computer_choice)
+            #   user_choice = user_choice.to_sym
+            #   if user_choice == computer_choice
+            #     return "Tie!"
+            #   elsif user_choice == $wining_states_hash[computer_choice]
+            #     return "Computer wins!"
+            #   elsif user_choice != $wining_states_hash[computer_choice]
+            #     return "User wins!"
+            #   end
+            # end
+
+            # get '/play/:user_choice' do
+            #   @user_choice = params[:user_choice]
+            #   @computer_choice = computer_choses()
+            #   @result = evaluate_results(@user_choice, @computer_choice)
+            #   erb :result
+            # end
+
+
 
 
 
@@ -29,9 +56,6 @@ $wins_hash = {
 get '/play' do
 
 # Convert each key selection to the value for both the user and computer
-
-
-
 computer_choice = $wins_hash.keys.sample
 user_choice = params[:selection].to_sym
 
